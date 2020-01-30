e7fb18e5a92d9704dacc2381a2d9297b60553b4f
kharding85 <52115620+kharding85@users.noreply.github.com>
2020-01-29 17:18:17 -0700

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+
+# get all lines from stdin
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    #make lower case
+    line = line.lower()
+
+    # split the line into words; splits on any whitespace
+    words = line.split()
+
+    # output tuples (word, 1) in tab-delimited format
+    stopwords = set(['the', 'and'])
+    for word in words:
+        if word not in stopwords:
+        print '%s\t%s' % (word, "1")
